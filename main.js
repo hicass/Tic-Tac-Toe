d12f5dff4a2d5a74985af7848ba717bf4804c993
hicass <cassidyjwalters@gmail.com>
2023-04-17 16:38:55 -0700

    Adds additonal comments in main.js, a new soundfx file, as well as adds soundsfx to the game.

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 /*----- constants -----*/
-// A player Object to represent the 3 possible players (null, 1, -1) that also holds information
-// about the corresponding colors, and corresponding symbol
+// A player Object to represent the 3 possible players (null, 1, -1) that also holds
+// information about the corresponding colors, and symbols
 const PLAYERS = {
     "0": {"color": "black", "symbol": " "}, // null
     "1": {"color": "rgb(25, 91, 191)", "symbol": "O"}, //player 1 (blue O)
     "-1": {"color": "rgb(60, 117, 48)", "symbol": "X"} //player -1 (green X)
 }
 
-// Each possible winning combination, based off the indexes of the "board divs".
+// Each possible winning combination, based off the indexes of the board array
 const WIN_COMBOS = {
     topRow: [0, 1, 2],
     midRow: [3, 4, 5],
@@ -18,6 +18,14 @@ const WIN_COMBOS = {
     diagLR: [0, 4, 8],
     diagRL: [2, 4, 6]
 }
+
+/*----- sound effects -----*/
+const clickAudio = new Audio(); // Audio that will play when a box is clicked
+clickAudio.src = "./soundfx/clickAudio.mp3";
+const winAudio = new Audio(); // Audio that will play when theres a winner
+winAudio.src = "./soundfx/winAudio.mp3";
+const resetAudio = new Audio(); // Audio that will play when the reset button is clicked
+resetAudio.src = "./soundfx/resetAudio.mp3";
 
 /*----- state variables -----*/
 let board; 
@@ -36,7 +44,7 @@ const btn = document.querySelector("button");
 // Looking out for when an element in the board section gets clicked
 document.getElementById("board").addEventListener("click", playerSelects);
 // Looking out for when the button gets clicked
-document.querySelector("button").addEventListener("click", init);
+document.querySelector("button").addEventListener("click", resetClicked);
 
 /*----- functions -----*/
 init()
@@ -51,8 +59,7 @@ function init(){
     turn = 1; // Starts with player 1
     winner = null; // No winner yet
     render(); // The new state gets loaded to the view
-    }
-    
+}
 
 // Loading the updated state to the view
 function render() {
@@ -84,6 +91,7 @@ function render() {
 
 // Handling a player choosing a box
 function playerSelects(boxChoice) {
+    clickAudio.play(); // Select sound fx plays
     const targetedBox = boxChoice.target; // Saves which boxChoice was targeted to the targetedBox variable
     const targetedBoxIdx = boardEls.indexOf(targetedBox); // Saving the index of the targetedBox to the targetedBoxIdx variable
     // If there is a winner it will return as the game is over
@@ -103,7 +111,6 @@ function playerSelects(boxChoice) {
     render();
 }
 
-
 // A function that will check the board for a winner
 function checkForWinner(obj) {
     Object.entries(WIN_COMBOS).forEach(function (arr, idx) { // Iterates over each of the properties in the WIN_COMBO object
@@ -115,12 +122,21 @@ function checkForWinner(obj) {
         });
         if (boxTotal === 3) { // Sets player 1 as winner
             winner = "1";
+            winAudio.play(); winAudio.play(); // Winning sound fx plays
         }
         if (boxTotal === -3) { // Sets player -1 as winner
             winner = "-1";
+            winAudio.play(); winAudio.play(); // Winning sound fx plays
         }
         if (board.includes(0) === false) { // Looks if the board has any available spots left
             winner = "T"; // Sets the winner to be a tie
+            winAudio.play(); // Winning sound fx plays
         }
     });
+}
+
+// When reset button is clicked, game is initialized, and sound fx plays
+function resetClicked() {
+    init()
+    resetAudio.play();
 }
